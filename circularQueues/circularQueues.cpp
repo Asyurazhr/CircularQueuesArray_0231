3e87b0f0afe55ee877eeda018ca7c04282c3b51b
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-30 15:06:07 +0700

    cek apakah antrian penuh

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -19,7 +19,13 @@ public:
 		cin >> num;
 		cout << endl;
 
-	
+	//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overlow\n";
+			return;
+	}
+
+
 
 int main()
 {
