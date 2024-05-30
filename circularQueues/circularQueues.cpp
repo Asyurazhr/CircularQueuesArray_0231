f22777fd941f482db3d982e150dd14831120fd7e
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-30 15:28:23 +0700

    iterasi dari awal array hingga rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -93,7 +93,14 @@ public:
 
 			FRONT_Position = 0;
 
-			
+			//iterasi dari awal array hingga REAR
+			while (FRONT_Position <= REAR_Position) {
+				cout << queue_array[FRONT_Position] << "  ";
+				FRONT_Position++;
+			}
+
+		}
+	}
 };
 
 
